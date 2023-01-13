292381eebfeb5583c49437a88cbe5530d31eebea
adanilkov <adanilkovas@gmail.com>
2023-01-13 04:43:42 -0500

    Added more product post actions

--- routes/posts.js
+++ routes/posts.js
@@ -3,17 +3,28 @@ const router = express.Router();
 const Post = require('../models/Post');
 
 //GET
-router.get('/', (req, res) => {
-    res.send('we are on posts');
+//get all
+router.get('/', async (req, res) => {
+    try {
+        const posts = Post.find();
+        //add limit/pagination later
+    } catch(err) {
+        res.status(500).json({message: err});
+    }
 });
-
-router.get('/item', (req, res) => {
-    res.send('a particular item\'s post');
+//get by id
+router.get('/:postId', async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.status(200).json(post);
+    } catch {
+        res.status(500).json({message: err});
+    }
 });
-
 
 //POST
-router.post('/', (req, res) => {
+//add a product post
+router.post('/', async (req, res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description,
@@ -21,9 +32,26 @@ router.post('/', (req, res) => {
         currentBid: req.body.currentBid,
         buyoutPrice: req.body.buyoutPrice,
         timeRemaining: req.body.timeRemaining,
-        datePosted: date.datePosted
+        status: req.body.status
     });
-})
+    try {
+        const savedPost = await post.save();
+        res.status(200).json(savedPost);
+    } catch(err){
+        res.status(500).json({message: err});
+        console.log(err);
+    }
+});
+
+//DELETE
+router.delete('/:postId', async(req, res) => {
+    try {
+        const removed = await Post.remove(req.params.postId);
+        res.status(200).json(removed);
+    } catch {
+        res.status(500).json({message: err});
+    }
+});
 
 //export to app
 module.exports = router;
